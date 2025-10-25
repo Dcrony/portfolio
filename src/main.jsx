221cc7b8e766f393bcba1df86e71a9ec525614d8
Dcrony <ibrahimabdulmajeed14@gmail.com>
2025-10-25 19:26:26 +0100

    delete unusefull page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PortfolioSingle from "./pages/PortfolioSingle";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -13,7 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/project/:id" element={<PortfolioSingle />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
